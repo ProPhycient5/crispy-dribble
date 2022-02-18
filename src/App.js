bc29eedc60f55304753efd73881bff32f265ef6e
sawan <sawan.d@mantralabsglobal.com>
2022-02-18 14:17:12 +0530

    changed text-color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl text-green-500 font-bold">
       Hello world! I am Sawan
     </h1>
   );
